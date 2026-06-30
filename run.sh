a441f814d334a4516ee05a86f80ee8dfaa6717a2
unique-chan <yechankim@gm.gist.ac.kr>
2026-06-30 21:53:07 +0900

    +

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
+# Text-guided image translation mode
+python infer_custom.py \
+    --rgb-dir example \
+    --output-dir preds_new \
+    --llava-base-path weights/llava-v1.5-7b \
+    --llava-lora-path weights/TherA_VLM \
+    --llava-prompt "how would this RGB scene appear in long-wave thermal infrared spectrum." \
+    --checkpoint weights \
+    --llava-device cuda:1
+
+
+# Reference-guided image translation mode
 python infer_example_guided.py \
   --mode two-image \
   --reference-image example/fig1.jpg \
@@ -7,4 +19,6 @@ python infer_example_guided.py \
   --llava-lora-path weights/TherA_VLM \
   --checkpoint weights  \
   --llava-device cuda:1
+
+# 
 
